94034fddd459bc256649986f8fa2fa91d4463e68
Szymon Czulak <szymonczulak1@gmail.com>
2021-11-30 21:31:58 +0100

    shader class

--- src/VertexArray.cpp
+++ src/VertexArray.cpp
@@ -13,7 +13,7 @@ void VertexArray::Bind() const
 	GLCall(glBindVertexArray(m_RendererID));
 }
 
-void VertexArray::UnBind() const
+void VertexArray::Unbind() const
 {
 	GLCall(glBindVertexArray(0));
 }
